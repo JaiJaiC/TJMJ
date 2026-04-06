55c15a1bc6a0a1a3c34742b20e7a71424b4fbfaa
JaiJaiC <1121015441@qq.com>
2026-04-06 22:47:55 +0800

    终极修复：使用 getImg 动态读取环境，彻底消灭 404 与白屏

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/TJMJ/'  // <--- 必须是这个！专门适配你的 GitHub Pages 路径
+  base: '/TJMJ/'  // 保持这个绝对正确的路径！
 })
