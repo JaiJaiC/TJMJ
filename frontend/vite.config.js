3ff0115f2042cdb6309bd964c96457071c35ebb4
JaiJaiC <1121015441@qq.com>
2026-04-06 22:57:40 +0800

    终极绝杀：配置正确的 base 路径，修正 JS 和 CSS 的 404

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/TJMJ/'  // 保持这个绝对正确的路径！
+  // 必须加上这个斜杠！明确告诉浏览器所有 JS 和 CSS 都在 /TJMJ/ 目录下找！
+  base: '/TJMJ/'
 })
