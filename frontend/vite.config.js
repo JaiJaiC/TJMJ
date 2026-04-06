27fa1da5b9e4e01f6cb2ab82bdd8246d2d88be72
JaiJaiC <1121015441@qq.com>
2026-04-06 22:35:24 +0800

    修复路径大满贯，根治白屏

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // 【最关键的一行】加上这个相对路径，打包后的文件就永远不会迷路了！
-  base: './'
+  base: '/TJMJ/'  // <--- 必须是这个！专门适配你的 GitHub Pages 路径
 })
